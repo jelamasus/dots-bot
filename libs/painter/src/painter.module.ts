1f3ee1318c22a810b3058d06ab260b72a7d5dc93
Vladislav Korenyuk <vladislav.korenyuk@hotger.com>
2024-10-21 17:18:08 +0300

    WIP

--- libs/painter/src/painter.module.ts
+++ libs/painter/src/painter.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
 import { PainterService } from './painter.service';
+import { readFileSync } from 'fs';
+import { join } from 'path';
+
+export const MAP_IMAGE = Symbol('Map image');
 
 @Module({
-  providers: [PainterService],
+  providers: [
+    {
+      provide: MAP_IMAGE,
+      useValue: readFileSync(join(__dirname, '..', 'assets', 'map.jpg')),
+    },
+    PainterService,
+  ],
   exports: [PainterService],
 })
 export class PainterModule {}
--- libs/painter/src/painter.service.ts
+++ libs/painter/src/painter.service.ts
@@ -1,4 +1,41 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { MAP_IMAGE } from './painter.module';
+import { CanvasRenderingContext2D, createCanvas, loadImage } from 'canvas';
+
+const START_ANGLE = Math.PI * 2;
 
 @Injectable()
-export class PainterService {}
+export class PainterService {
+  constructor(@Inject(MAP_IMAGE) private readonly mapImage: Buffer) {}
+
+  private async create() {
+    const image = await loadImage(this.mapImage);
+    const canvas = createCanvas(image.width, image.height);
+    const ctx = canvas.getContext('2d');
+    ctx.drawImage(image, image.width, image.height);
+
+    return {
+      canvas,
+      ctx,
+    };
+  }
+
+  private setDot({
+    ctx,
+    center,
+    radius,
+  }: {
+    ctx: CanvasRenderingContext2D;
+    center: [number, number];
+    radius: number;
+  }): Promise<void> {
+    return new Promise((resolve) => {
+      ctx.beginPath();
+      ctx.arc(center[0], center[1], radius, START_ANGLE, 0);
+      ctx.fillStyle = 'black';
+      ctx.fill();
+
+      resolve();
+    });
+  }
+}
